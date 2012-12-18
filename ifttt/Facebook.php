b0dba8de9ff65db38d82240be49c67e50d20556b
John Branca <john.branca@gmail.com>
2012-12-18 13:55:52 -0500

    Get rid of sendgrid, use gmail to send email

--- ifttt/Facebook.php
+++ ifttt/Facebook.php
@@ -1,26 +1,13 @@
 <?php
 
-require_once 'sendgrid-php/SendGrid_loader.php';
-
 class Facebook 
 {
-	private $sendgrid;
-
 	public function __construct()
-	{
-		$this->sendgrid = new SendGrid('jbranca', 'chowthis');
-	}
+	{}
 
 	public function sendMail($message)
 	{
-		$mail = new SendGrid\Mail();
-		$mail->addTo('jbranca@facebook.com')
-			->setFrom('alerts2.0@espn.com')
-			->setSubject('Gunt')
-			->setText($message)
-			->setHtml($message);
-
-		$this->sendgrid->smtp->send($mail);
+		mail('jbranca@facebook.com', 'Subject', $message, "From: alerts2.0@espn.com");
 
 		echo 'Email Sent';
 
