dd605214544e4b5fbb3fee4b5596a94a17fbbf14
John Branca <john.branca@gmail.com>
2012-12-18 17:32:04 -0500

    Post to user's facebook wall

--- ifttt/Alerts_Facebook.php
+++ ifttt/Alerts_Facebook.php
@@ -4,15 +4,29 @@ require_once 'facebook-php-sdk/src/facebook.php';
 
 class Alerts_Facebook 
 {
-	const APP_ID = '259665347494601';
-	const APP_SECRET = '78c9a12d5aff0a2a36823cfee529e18d';
+    const APP_ID = '259665347494601';
+    const APP_SECRET = '78c9a12d5aff0a2a36823cfee529e18d';
 
-	public function __construct()
-	{
-		$this->facebook = new Facebook(array(
-			'appId'  => self::APP_ID,
-			'secret' => self::APP_SECRET,
-			'cookie' => true
-		));
-	}
+    public function __construct()
+    {
+        $this->facebook = new Facebook(array(
+            'appId'  => self::APP_ID,
+            'secret' => self::APP_SECRET,
+            'cookie' => true
+        ));
+    }
+
+    public function postWall()
+    {
+        $userId = $this->facebook->getUser();
+
+        $parameters = array(
+            'access_token'  => $this->facebook->getAccessToken(),
+            'message'       => "Test message",
+            'from'          => self::APP_ID,
+            'to'            => $userId
+        );
+
+        $this->facebook->api("/" . $userId . "/feed", "post", $parameters);
+    }
 }
