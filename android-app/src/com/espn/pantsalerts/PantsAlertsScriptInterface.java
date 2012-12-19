69434d4a047bd8236ce4993e518e0d2b4c155aae
Mike Marrone <marrone.mike@gmail.com>
2012-12-19 10:05:13 -0500

    camera strobe as separate thread; ability to request stopping of strobe; fix for manipulating camera torch during strobe

--- android-app/src/com/espn/pantsalerts/PantsAlertsScriptInterface.java
+++ android-app/src/com/espn/pantsalerts/PantsAlertsScriptInterface.java
@@ -16,7 +16,63 @@ public class PantsAlertsScriptInterface {
 	private TextToSpeech mTts;
 	private Camera mCamera;
 	private Camera.Parameters mCameraParameters;
-	public volatile boolean isStrobeRunning = false;
+	private StrobeRunner strobeRunner = new StrobeRunner();
+	private Thread strobeThread;
+
+	private class StrobeRunner implements Runnable {
+		public volatile boolean isRunning = false;
+		public volatile boolean stopRequested = false;
+		public volatile int delay = 10;
+		public volatile int delayOff = 500;
+		public volatile int numFlashes = 5;
+		public volatile Camera mCamera;
+
+		public StrobeRunner() { }
+
+		public void run() { 
+			if(isRunning) {
+				return;
+			}
+			
+			stopRequested = false;
+			isRunning = true;
+
+			mCamera = Camera.open();
+			
+			Camera.Parameters pon = mCamera.getParameters();
+			Camera.Parameters poff = mCamera.getParameters();
+			pon.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+			poff.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+			
+			int count = 0;
+			while(count++ < numFlashes && !stopRequested) {
+				try{
+					if(mCamera != null) {
+						mCamera.setParameters(pon);
+						Thread.sleep(delay);
+					}
+					if(mCamera != null) { 
+						mCamera.setParameters(poff);
+						Thread.sleep(delayOff);
+					}
+				}
+				catch(InterruptedException ex) {
+				}
+				catch(RuntimeException ex) {
+					stopRequested = true;
+				}
+			}
+			
+			if(mCamera != null) {
+				mCamera.release();
+				mCamera = null;
+			}
+			
+			isRunning = false;
+			stopRequested = false;
+		}
+	}
+
 
     /** Instantiate the interface and set the context */
     PantsAlertsScriptInterface(Context c, WebView view, TextToSpeech tts) {
@@ -46,6 +102,11 @@ public class PantsAlertsScriptInterface {
 	}
 
 	public void setCameraTorchOn() {
+		if(strobeRunner.isRunning) {
+			strobeRunner.stopRequested = true;
+			strobeRunner.mCamera.release();
+			strobeRunner.mCamera = null;
+		}
 		if(mCamera == null) {
 			mCamera = Camera.open();
 			mCameraParameters = mCamera.getParameters();
@@ -55,6 +116,11 @@ public class PantsAlertsScriptInterface {
 	}
 
 	public void setCameraTorchOff() {
+		if(strobeRunner.isRunning) {
+			strobeRunner.stopRequested = true;
+			strobeRunner.mCamera.release();
+			strobeRunner.mCamera = null;
+		}
 		if(mCamera != null) {
 			mCameraParameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
 			mCamera.setParameters(mCameraParameters);
@@ -73,43 +139,24 @@ public class PantsAlertsScriptInterface {
 	}
 
 	public void startCameraStrobe(int numFlashes, int strobeDelay, int strobeDelayOff) {
-    	if(isStrobeRunning) {
-    		return;
+		if(mCamera != null) { 
+			mCamera.release();
+			mCamera = null;
+			mCameraParameters = null;
 		}
-    	
-    	//requestStrobeStop = false;
-    	isStrobeRunning = true;
-    	
-		if(mCamera == null) { 
-			mCamera = Camera.open();
+
+		strobeRunner.delay = strobeDelay;
+		strobeRunner.delayOff = strobeDelayOff;
+		strobeRunner.numFlashes = numFlashes;
+		strobeRunner.stopRequested = false;
+		if(!strobeRunner.isRunning) { 
+			strobeThread = new Thread(strobeRunner);
+			strobeThread.start();
 		}
-		
-    	Camera.Parameters pon = mCamera.getParameters();
-		Camera.Parameters poff = mCamera.getParameters();
-    	pon.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-    	poff.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
-    	
-		int count = 0;
-    	while(count++ < numFlashes) {// && !requestStrobeStop) {
-    		try{
-        		mCamera.setParameters(pon);
-        		Thread.sleep(strobeDelay);
-        		mCamera.setParameters(poff);
-        		Thread.sleep(strobeDelayOff);
-    		}
-    		catch(InterruptedException ex) {
-    		}
-    		catch(RuntimeException ex) {
-    			//requestStrobeStop = true;
-    		}
-    	}
-    	
-    	mCamera.release();
-		mCamera = null;
-		mCameraParameters = null;
-    	
-    	isStrobeRunning = false;
-    	//requestStrobeStop = false;
+	}
+
+	public void requestStopCameraStrobe() {
+		strobeRunner.stopRequested = true;
 	}
 
 }
