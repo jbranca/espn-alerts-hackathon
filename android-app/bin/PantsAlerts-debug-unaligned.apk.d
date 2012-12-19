7b8e6758cdc0dd34164a166774edb5cce850ac77
Mike Marrone <marrone.mike@gmail.com>
2012-12-18 21:05:16 -0500

    new android app builds with strobe effect

--- android-app/bin/PantsAlerts-debug-unaligned.apk.d
+++ android-app/bin/PantsAlerts-debug-unaligned.apk.d
@@ -1,4 +1,4 @@
-/home/mike/projects/hackathon/espn-alerts-hackathon/android-app/bin/PantsAlerts-debug-unaligned.apk : \
-/home/mike/projects/hackathon/espn-alerts-hackathon/android-app/bin/PantsAlerts.ap_ \
-/home/mike/projects/hackathon/espn-alerts-hackathon/android-app/bin/classes.dex \
-/home/mike/android-sdk-linux/tools/support/annotations.jar \
+/home/mike/projects/espn/hackathon/espn-alerts-hackathon/android-app/bin/PantsAlerts-debug-unaligned.apk : \
+/home/mike/projects/espn/hackathon/espn-alerts-hackathon/android-app/bin/PantsAlerts.ap_ \
+/home/mike/projects/espn/hackathon/espn-alerts-hackathon/android-app/bin/classes.dex \
+/home/mike/bin/android-sdk-linux/tools/support/annotations.jar \
